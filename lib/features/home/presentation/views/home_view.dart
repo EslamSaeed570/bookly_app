cf489d3593b62efec4823529fd95ea2a1e9611fa
EslamSaeed570 <solomelprens12345@gmail.com>
2025-08-15 10:35:36 +0300

    create custom app bar to home view

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,4 +1,5 @@
-import 'package:bookly_app/core/utils/assets.dart';
+
+import 'package:bookly_app/features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -6,18 +7,8 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Image.asset(
-          AssetsData.logo,
-          height: 80,
-          width: 100,
-        ),
-        actions:const [
-          Icon(Icons.search),
-        ],
-      ),
+    return const Scaffold(
+     body: HomeViewBody(),
     );
   }
 }
